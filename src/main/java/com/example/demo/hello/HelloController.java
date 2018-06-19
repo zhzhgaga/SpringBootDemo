32154faac259a4dbccfca902fd63030df8168117
gabriel zhou <gabriel.zhou@morningstar.com>
2018-06-19 14:10:38 +0800

    update

---
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -0,0 +1,9 @@
+package com.example.demo.hello;
+
+/**
+ * @author gzhou2
+ * @date 2018/6/19 14:04
+ */
+
+public class HelloController {
+}
---
+++ src/main/java/com/example/demo/properties/PropertiesController.java
@@ -0,0 +1,9 @@
+package com.example.demo.Properties;
+
+/**
+ * @author gzhou2
+ * @date 2018/6/19 13:55
+ */
+
+public class PropertiesController {
+}
