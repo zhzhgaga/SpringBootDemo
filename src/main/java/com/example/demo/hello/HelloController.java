07604391095ca4b9ec3bc0d68b42467d19f4c257
gabriel zhou <gabriel.zhou@morningstar.com>
2018-07-09 14:22:19 +0800

    update

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -3,7 +3,6 @@ package com.example.demo.hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author gzhou2
@@ -16,7 +15,7 @@ public class HelloController {
 
 
     @GetMapping("/sayHello")
-    public String hello(){
+    public String hello() {
         return "Hello Spring boot.";
     }
 
